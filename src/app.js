147c4658e2aa31f05a581114eb8f4e8a08530db7
Ryszard Matula <rmatula@us.edu.pl>
2021-01-28 15:56:58 +0100

    Kinda working XD

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
+const asyncHandler = require("express-async-handler");
 const morgan = require("morgan");
-const { selectAllEmployees } = require("./sql");
+const { selectAllEmployees, getEmployee } = require("./sql");
 
 // settings
 app.set("port", process.env.PORT || 3000);
@@ -13,8 +14,20 @@ app.set("views", path.join(__dirname, "views"));
 // middlewares
 app.use(morgan("dev"));
 
-app.get("/employees", (req, res) => {
-  selectAllEmployees(req, res);
+app.get("/", (req, res) => {
+  data = selectAllEmployees(req, res);
+  res.render("home", {
+    title: "Ryszard Matula - ZIIBD - IO1",
+    users: data,
+  });
+});
+
+app.get("/edit/:employeeID", (req, res) => {
+  data = getEmployee(req, res);
+  res.render("employee", {
+    title: "Ryszard Matula - ZIIBD - IO1",
+    data,
+  });
 });
 
 // routes
--- src/sql/index.js
+++ src/sql/index.js
@@ -13,6 +13,45 @@ module.exports = {
       console.log("connected to database");
       // run query to get all employees
       result = await connection.execute(`SELECT * FROM employees`);
+      console.log(result);
+    } catch (err) {
+      //send error message
+      return res.send(err.message);
+    } finally {
+      if (connection) {
+        try {
+          // Always close connections
+          await connection.close();
+          console.log("close connection success");
+        } catch (err) {
+          console.error(err.message);
+        }
+      }
+      if (result.rows.length == 0) {
+        //query return zero employees
+        return res.send("query send no rows");
+      } else {
+        //send all employees
+        return res.send(result.rows);
+      }
+    }
+  },
+  getEmployee: async function getEmployee(req, res) {
+    console.log(req.params.employeeID);
+    try {
+      connection = await oracledb.getConnection({
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        connectString: process.env.CONNECT_STRING,
+      });
+
+      //
+      console.log("connected to database");
+      // run query to get all employees
+      result = await connection.execute(
+        `SELECT * FROM employees WHERE employee_id = ${req.params.employeeID}`
+      );
+      console.log(result);
     } catch (err) {
       //send error message
       return res.send(err.message);
