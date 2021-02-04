b1cbcdbeacbaeec783cf07641633239ec4309751
Ryszard Matula <rmatula@us.edu.pl>
2021-02-04 19:04:46 +0100

    Yikes

--- src/app.js
+++ src/app.js
@@ -13,66 +13,72 @@ const {
 } = require("./sql");
 const bodyParser = require("body-parser");
 
-// settings
-app.set("port", process.env.PORT || 3000);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+const main = async () => {
+  // settings
+  app.set("port", process.env.PORT || 3000);
+  app.set("view engine", "ejs");
+  app.set("views", path.join(__dirname, "views"));
 
-// middlewares
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: true }));
+  // middlewares
+  app.use(morgan("dev"));
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
-// routes
-app.get("/", (req, res) => {
-  data = selectAllEmployees(req, res);
-  res.render("home", {
-    title: "Ryszard Matula - ZIIBD - IO1",
-    users: data,
+  // routes
+  app.get("/", (req, res) => {
+    data = selectAllEmployees(req, res);
+    res.render("home", {
+      title: "Ryszard Matula - ZIIBD - IO1",
+      users: data,
+    });
   });
-});
 
-app.get("/edit/:employeeID", (req, res) => {
-  data = getEmployee(req, res);
-  res.render("employee", {
-    title: "Ryszard Matula - ZIIBD - IO1",
-    data,
+  app.get("/edit/:employeeID", (req, res) => {
+    data = getEmployee(req, res);
+    res.render("employee", {
+      title: "Ryszard Matula - ZIIBD - IO1",
+      data,
+    });
   });
-});
 
-app.post(
-  "/edit/:employeeID",
-  asyncHandler(async (req, res) => {
-    console.log(req.body);
-    try {
-      await editEmployee(req, res);
-    } catch (e) {
-      console.log(e);
-    }
-  })
-);
+  app.post(
+    "/edit/:employeeID",
+    asyncHandler(async (req, res) => {
+      console.log(req.body);
+      try {
+        await editEmployee(req, res);
+      } catch (e) {
+        console.log(e);
+      }
+    })
+  );
 
-app.get("/add-employee", (req, res) => {
-  res.render("add-employee");
-});
+  app.get("/add-employee", (req, res) => {
+    res.render("add-employee");
+  });
 
-app.post(
-  "/add-employee",
-  asyncHandler(async (req, res) => {
-    try {
-      await addEmployee(req, res);
-    } catch (e) {
-      console.log(e);
-    }
-  })
-);
+  app.post(
+    "/add-employee",
+    asyncHandler(async (req, res) => {
+      try {
+        await addEmployee(req, res);
+      } catch (e) {
+        console.log(e);
+      }
+    })
+  );
 
-app.get("/delete/:employeeID", (req, res) => {
-  deleteEmployee(req, res);
-});
+  app.get("/delete/:employeeID", (req, res) => {
+    deleteEmployee(req, res);
+  });
 
-// routes
-app.listen(app.get("port"), () => {
-  console.log("Server on port 3000");
+  // routes
+  app.listen(app.get("port"), () => {
+    console.log("Server on port 3000");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
 });
